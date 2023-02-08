a5d019a430b440ca3a4a1c1f5ef2f242ee0a0276
Rich Henry <roatanrich@gmail.com>
2023-02-07 18:56:28 -0700

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,22 @@
 import cors from 'cors'
 import 'dotenv/config'
 import express from 'express'
+import fs from 'fs'
 import morganMiddleware from './config/morganMiddleware'
 import Logger from "./lib/logger"
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 
-export const delayMillis = (delayMs: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delayMs));
+const app = express()
+const PORT = process.env.PORT
 
-export const greet = (name: string): string => `Hello ${name}`
-
-export const foo = async (): Promise<boolean> => {
-  console.log(greet('World'))
-  await delayMillis(1000)
-  console.log('done')
-  return true
-}
-
-const app = express();
-
-const PORT = process.env.PORT;
+const customCss = fs.readFileSync((process.cwd() + "/swagger.css"), 'utf8');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morganMiddleware)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCss }));
 
 app.get("/", (_, res) => {  
   res.redirect(`http://localhost:${PORT}/logger`);
